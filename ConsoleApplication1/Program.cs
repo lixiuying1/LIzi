a1d7617cb8c610489cf7862bdf46c985c63cec5e
lixiuying1 <1660412195@qq.com>
2017-04-10 11:14:32 +0800

    第一次提交

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlSearch = "select * from Myschool1";
+            List<Userinfo> re = DBHelper.Query<Userinfo>(sqlSearch, sdr =>
+            {
+                Userinfo p = new Userinfo();
+                p.ID = (int)sdr["ID"];
+                p.Namw = sdr["Namw"].ToString();
+
+                return p;
+            });
+            foreach (var item in re)
+            {
+                Console.WriteLine(item.ID);
+                Console.WriteLine(item.Namw);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
